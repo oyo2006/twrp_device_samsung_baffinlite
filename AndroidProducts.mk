efaa87f2ebcd988625dd0f207bf7558680e5c86a
oyo2006 <omranmansour097@gmail.com>
2021-11-28 20:43:02 +0200

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_baffinlite.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_baffinlite-user \
+    omni_baffinlite-userdebug \
+    omni_baffinlite-eng
